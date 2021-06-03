f03f7025e1ed9a3a0f0373e9a5778c9585fbe5d2
Ivanchenko59 <Ivanchenko59@gmail.com>
2021-06-04 01:59:46 +0300

    Check Whether a Number is Positive or Negative or Zero

--- Topic2/Task6.c
+++ Topic2/Task6.c
@@ -2,7 +2,18 @@
 
 int main() {
 
-    printf("Hello ");
+    double number;
+    
+    printf("Enter the number: ");
+    scanf("%lf", &number);
+
+    if (number > 0) 
+        printf("%.2lf - is positive", number);
+    else if (number < 0) 
+        printf("%.2lf - is negative", number);
+
+    else
+        printf("%.0lf - is zero", number);
 
     return 0;
 }
