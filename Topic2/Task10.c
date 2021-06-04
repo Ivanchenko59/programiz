c3997abe4b0a96f68ed1015db84bcd72dc3b43d6
Ivanchenko59 <Ivanchenko59@gmail.com>
2021-06-04 17:08:51 +0300

    Generate multiplication table

---
+++ Topic2/Task10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i <= 10; i++) {
+
+        printf("%d * %d = %d \n", n, i, n * i);
+    }
+    
+    return 0;
+}
