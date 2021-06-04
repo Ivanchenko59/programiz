4f5c0e2a67e4f360f35d5000b756b574979aee2e
Ivanchenko59 <Ivanchenko59@gmail.com>
2021-06-04 18:33:22 +0300

    Find HCF of two numbers

---
+++ Topic2/Task12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    int i, n1, n2, gcd;
+ 
+    printf("Enter the two number: ");
+    scanf("%d %d", &n1, &n2);
+
+    for (i = 1; i <= n1 && i <= n2; i++) {
+        if (n1 % i == 0 && n2 % i == 0)
+            gcd = i;
+    }
+    
+    printf("GCD of %d and %d is %d", n1, n2, gcd);
+
+    return 0;
+}
