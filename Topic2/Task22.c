1d347359a79bc6d5d56ca2ee23e28787a3d043a9
Ivanchenko59 <Ivanchenko59@gmail.com>
2021-06-05 19:15:48 +0300

    Display factors of a number

---
+++ Topic2/Task22.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    printf("Factor of %d are: ", n);
+
+    for (int i = 1; i <= n; i++) {
+
+        if (n % i == 0) 
+            printf("%d, ", i);
+
+    }
+
+    return 0;
+}
