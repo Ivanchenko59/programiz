0214de1380f2e29c5ded8ed0b5301152eeed047a
Ivanchenko59 <Ivanchenko59@gmail.com>
2021-06-06 16:15:33 +0300

    Find the sum of natural numbers using recursion

---
+++ Topic3/Task4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int addNumbers(int n); 
+
+int main() {
+
+    int num;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    printf("Sum = %d",addNumbers(num));
+
+    return 0;
+}
+
+int addNumbers(int n) {
+
+    if (n != 0)
+        return n + addNumbers(n-1);
+    else
+        return n; 
+}
