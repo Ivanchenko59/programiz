d866b15493dd0c35dfc102fff6824e8b0135ea21
Ivanchenko59 <Ivanchenko59@gmail.com>
2021-06-10 21:08:00 +0300

    Three-dimensional array

---
+++ Task4/Task3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    
+    int arr[2][3][2];
+
+    printf("Enter 12 values: \n");
+
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            for (int k = 0; k < 2; ++k) {
+                scanf("%d", &arr[i][j][k]);
+            }
+        }
+    }
+
+  
+  printf("\nDisplaying values:\n");
+  
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            for (int k = 0; k < 2; ++k) {
+                printf("arr[%d][%d][%d] = %d\n", i, j, k, arr[i][j][k]);
+            }
+        }
+    }
+
+  return 0;
+}
